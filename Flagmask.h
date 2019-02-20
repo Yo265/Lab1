459f79b7cd13101c87158b5ad4cd48cc6b7f6129
Yo265 <32211986+Yo265@users.noreply.github.com>
2019-02-21 03:20:44 +0700

    Created main headers

---
+++ Flagmask.h
@@ -0,0 +1,36 @@
+#ifndef _FLAG_MASK_
+#define _FLAG_MASK_
+
+#define AOF 0	// Out of Range flag
+#define flag1 1	//
+#define flag2 2	//
+#define flag3 3	//
+#define flag4 4	//
+#define flag5 5	//
+#define flag6 6	//
+#define flag7 7	//
+#define flag8 8	//
+#define flag9 9	//
+#define flag10 10	//
+#define flag11 11	//
+#define flag12 12	//
+#define flag13 13	//
+#define flag14 14	//
+#define flag15 15	//
+#define flag16 16	//
+#define flag17 17	//
+#define flag18 19	//
+#define flag20 20	//
+#define flag21 21	//
+#define flag22 22	//
+#define flag23 23	//
+#define flag24 24	//
+#define flag25 25	//
+#define flag26 26	//
+#define flag27 27	//
+#define flag28 28	//
+#define flag29 29	//
+#define flag30 30	//
+#define flag31 31	//
+
+#endif //_FLAG_MASK_
---
+++ Mysimplecomputer.h
@@ -0,0 +1,31 @@
+#ifndef _MYSIMPLECOMPUTER_
+#define _MYSIMPLECOMPUTER_
+
+#include "Const.h"
+#include "Flagmask.h"
+#include "Commands.h"
+
+extern int mass[N];
+extern int registr;
+
+	/*Memory Function*/
+int sc_memoryInit(void);
+int sc_memorySet(int adress, int value);
+int sc_memoryGet(int adress, int* value);
+int sc_memorySave(char* filename);
+int sc_memoryLoad(char *filename);
+	/*Memory end*/
+	
+	/*FLAGS register Function*/
+int sc_regInit(void);
+int sc_regSet(int flag, int value);
+int sc_regGet(int flag, int * value);
+	/*FLAGS end*/
+
+	/*Ñommand processing functions*/
+int checkCommand(int command);
+int sc_commandEncode(int command, int operand, int * value);
+int sc_commandDecode(int value, int * command, int * operand);
+	/*Ñommand processing end*/
+	
+#endif //_MYSIMPLECOMPUTER_
