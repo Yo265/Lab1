98975c52fc30864428a4af1f68fba42d65e004ad
Yo265 <32211986+Yo265@users.noreply.github.com>
2019-02-21 03:22:30 +0700

    Create Register.cpp

---
+++ Register.cpp
@@ -0,0 +1,14 @@
+#include "Mysimplecomputer.h"
+
+int sc_regInit(void){
+	registr = registr & 0;
+	return 1;
+}
+
+int sc_regSet(int flag, int value){
+	
+}
+
+int sc_regGet(int flag, int * value){
+	
+}
